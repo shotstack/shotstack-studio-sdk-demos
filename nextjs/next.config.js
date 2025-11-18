835c3b9bbb4d32086dd7616c1e38ef9efef43bbf
dazzatronus <derkzomer@gmail.com>
2025-11-18 20:50:13 +1100

    chore: apply consistent formatting

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -1,28 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        path: false,
-        url: false,
-      };
-    }
+	reactStrictMode: false,
+	webpack: (config, { isServer }) => {
+		if (!isServer) {
+			config.resolve.fallback = {
+				...config.resolve.fallback,
+				fs: false,
+				path: false,
+				url: false
+			};
+		}
 
-    config.experiments = {
-      ...config.experiments,
-      asyncWebAssembly: true,
-    };
+		config.experiments = {
+			...config.experiments,
+			asyncWebAssembly: true
+		};
 
-    config.module.rules.push({
-      test: /\.wasm$/,
-      type: "webassembly/async",
-    });
+		config.module.rules.push({
+			test: /\.wasm$/,
+			type: "webassembly/async"
+		});
 
-    return config;
-  },
+		return config;
+	}
 };
 
 module.exports = nextConfig;
