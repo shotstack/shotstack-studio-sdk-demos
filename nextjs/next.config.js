1d691d67a34cd13244ecfd53aba00a7bdc7c24a4
Kratos2k7 <umernadeem321@gmail.com>
2025-11-21 16:52:30 +0500

    updated nextjs config

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -1,28 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: false,
-	webpack: (config, { isServer }) => {
-		if (!isServer) {
-			config.resolve.fallback = {
-				...config.resolve.fallback,
-				fs: false,
-				path: false,
-				url: false
-			};
-		}
-
-		config.experiments = {
-			...config.experiments,
-			asyncWebAssembly: true
-		};
-
-		config.module.rules.push({
-			test: /\.wasm$/,
-			type: "webassembly/async"
-		});
-
-		return config;
-	}
+  reactStrictMode: false,
 };
 
 module.exports = nextConfig;
